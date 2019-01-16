dd875c3c380d36391efaf4dfe18ff0c822ec4ce7
GoreBee <rihardborge@gmail.com>
2019-01-16 11:57:11 +0300

    \добавлено решение с помощью рекурсии

--- 2-4.py
+++ 2-4.py
@@ -1,11 +1,20 @@
 n=int(input("Введите число элементов ряда для суммирования: "))
 
+n1=n
 el = 1
 elsum = 0
 
-while n>0:
+while n1>0:
     elsum +=el
     el /= -2
-    n -=1
+    n1 -=1
 
-print(f"Сумма равна: {elsum}") 
+print(f"Решение с помощью цикла. Сумма равна: {elsum}") 
+
+def recur (n,m):
+    if n==m+1:
+        return (-0.5)**m
+    else:
+        return (-0.5)**m+recur(n,m+1)
+
+print(f"Решение с помощью рекурсии. Сумма равна: {recur(n,0)}") 
